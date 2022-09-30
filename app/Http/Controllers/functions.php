314e1591de950ac5aad36b8a419bb1fb9e5fef19
Sourayaak <sourayaabdelkader@gmail.com>
2022-09-30 21:02:58 +0300

    fixing the api which decomposes a number

--- app/Http/Controllers/functions.php
+++ app/Http/Controllers/functions.php
@@ -9,20 +9,29 @@ class functions extends Controller
 {
     // Create an API that recives a number $num and returns each place value in the number.
     function decomposingNumber($number){
+        $abs_number = abs($number);
         $number_decompotion = array();
+        $i = 1;
+        if ($number > 0){
         $length = strlen($number);
-        for ($i = 1 ; $i <= $length; $i++) {
-            $new_number = $number % (10**(length-i)); // to take the firts digit on the left of the number
-            $new_number = $new_number * (10**(length-i)); // multiply this digit to take it's place value 
+        } else {$length = strlen($number) -1;}
+        while($i <= $length) {
+            $place_value = 10**($length - $i);
+            echo $place_value;
+            $new_number = floor($abs_number/ $place_value); // to take the firts digit on the left of the number
+            $new_number = $new_number * $place_value; // multiply this digit to take it's place value 
+            if ($number > 0 ){
             array_push($number_decompotion, $new_number); 
-            $number = $number - $new_number; 
+            } else {array_push($number_decompotion, -$new_number);}
+            $abs_number = $abs_number - $new_number; 
+            $i++;
         };
 
         return response()->json([
             "status" => "Success",
             "message" => $number_decompotion
         ]);
-    };
+    }
 
     // function that translates from Human to Programer by replacing the numbers in a string with their binary form.
     function integerToBinary($string){
